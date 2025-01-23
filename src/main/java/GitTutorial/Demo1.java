4017fe9123519b507d9f7f6eebed022f810170c2
Rajkumar-2011 <bidarjnvonline@gmail.com>
2025-01-23 13:04:03 +0530

    new try catch block program added for catching the exception from the program

--- src/main/java/GitTutorial/Demo1.java
+++ src/main/java/GitTutorial/Demo1.java
@@ -1,9 +1,51 @@
 package GitTutorial;
 
 public class Demo1 {
-		
-	
 	public static void main(String[] args) {
-		System.out.println("this is new project created");
+		int a=10;
+		int b=0;
+		String c=null;
+		String d="selenium";
+		
+		try
+		{
+			System.out.println("Try block started");
+			
+			System.out.println(a/b);
+			System.out.println(c.length());
+			System.out.println(Integer.parseInt(d));
+			System.out.println("Try block Ended");
+		}
+		catch (ArithmeticException e) {
+			System.out.println("Catch Block 1 ");
+		}
+		catch (NumberFormatException e) {
+			System.out.println("Catch Block 2 ");
+		}
+		catch (NullPointerException  e) {
+			System.out.println("Catch Block 3 ");
+		}
+		catch (IndexOutOfBoundsException e) {
+			System.out.println("Catch Block 4 ");
+		}
+		catch (RuntimeException  e) {
+			System.out.println("Catch Block 5");
+		}
+		catch (Exception e) {
+			System.out.println("Catch Block 6 ");
+		}
+		catch (Throwable e) {
+			System.out.println("Catch Block 7 ");
+		}
+		finally
+		{
+			System.out.println("finally block of code");
+		}
+		
+		
 	}
+
+	
+	
+	
 }
